1ec2fe2f5111b896744ff51162df074e78d6c2db
Rodrigo Queipo <rodrigoq@gmail.com>
2021-06-08 21:37:01 -0300

    Add chmod.

--- src/PdfToHtml.php
+++ src/PdfToHtml.php
@@ -8,17 +8,26 @@ class PdfToHtml
 
 	public static function GetBin() : string
 	{
+		self::SetX();
 		return self::BIN;
 	}
 
 	public static function GetBinFullPath() : string
 	{
-		return self::GetPath() . '/' . self::BIN;
+		self::SetX();
+		return realpath(self::GetPath() . '/' . self::BIN);
 	}
 
 	public static function GetPath() : string
 	{
 		return realpath(__DIR__ . '/../bin');
+	}
+
+	public static function setX() : void
+	{
+		$file = realpath(self::GetPath() . '/' . self::BIN);
+		if(fileperms($file) & 0777 !== 0755)
+			chmod($file, 0755);
 	}
 }
 
